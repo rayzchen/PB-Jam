330418a553d6b830677596d573af51a8d6cc46cf
Ray Chen <tankimarshal2@gmail.com>
2020-07-25 19:35:02 +0100

    Add files via Git

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Transform cam;
+    public float speed = 5f;
+
+    Vector2 movement;
+    Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        cam.position = new Vector3(transform.position.x, transform.position.y, -10);
+    }
+
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
