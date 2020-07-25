f7e53e18f950cee909f81e97e7cd6b5d88b42cb2
Ray Chen <tankimarshal2@gmail.com>
2020-07-25 21:23:31 +0100

    Add files via Git

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -11,15 +11,18 @@ public class PlayerController : MonoBehaviour {
     Vector2 movement;
     Rigidbody2D rb;
     Vector3 velocity = Vector3.zero;
+    Camera camera;
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
+        camera = cam.gameObject.GetComponent<Camera>();
     }
 
     void Update() {
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
         cam.position = Vector3.SmoothDamp(cam.position, new Vector3(transform.position.x, transform.position.y, -10), ref velocity, 1 / camSpeed);
+        camera.orthographicSize *= 1 + 0.01f * Input.GetAxis("Z axis");
     }
 
     void FixedUpdate() {
