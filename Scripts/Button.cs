bc824fab1b93b3db473abf370cde5ce69a034570
Ray Chen <tankimarshal2@gmail.com>
2020-07-29 07:50:17 +0100

    Add files via Git

---
+++ Scripts/Button.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class Button : MonoBehaviour {
+
+    void Update() {
+        
+    }
+}
